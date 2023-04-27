bcd6439a95ecaa730ea2177a71c1a5e9f629031d
aarticontractor <contaaru@gmail.com>
2023-04-27 16:11:47 -0700

    Moved HTML Wildcard at the end and added GET API Route

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3002;
 
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // HTML Routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../notes.html'));
 });
 
-app.get('*', (req, res) => {
-    
-  res.sendFile(path.join(__dirname, '../../index.html'));
-});
-
+// API Routes for added notes page
+app.get('/api/notes', (req, res) => {
+    console.log("I am here");
+    const db_file = path.join(__dirname, '../../../db/db.json')
+    const notes = JSON.parse(fs.readFileSync(db_file, 'utf8'));
+    res.json(notes);
+  });
+  
+ 
+// Add HTML Wildcard route at the end
+  app.get('*', (req, res) => {
+      
+    res.sendFile(path.join(__dirname, '../../index.html'));
+  });
+  
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
